5b99a35174280ad0c34271288681f92b9f6e73d1
James Wigger <james@rootstudio.co.uk>
2015-11-18 16:44:01 +0000

    Added API key to google geocoder

--- addons/apps/jw_stockists/classes/JwStockists_Geocode.class.php
+++ addons/apps/jw_stockists/classes/JwStockists_Geocode.class.php
@@ -6,7 +6,15 @@ class JwStockists_Geocode
 {
     public static function geocode($address)
     {
+        $API = new PerchAPI(1.0, 'jw_stockists');
+        $Settings = $API->get('Settings');
+        $api_key = $Settings->get('jw_stockists_google_api_key')->val();
+
         $Geocoder = new GoogleMapsGeocoder($address);
+
+        if($api_key) {
+            $Geocoder->setApiKey($api_key);
+        }
 
         return $Geocoder->geocode();
     }
