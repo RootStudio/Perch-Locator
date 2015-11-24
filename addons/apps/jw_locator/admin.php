4e497b3086c53588dbd6c6ed3339c601c6c009c1
James Wigger <james@rootstudio.co.uk>
2015-11-24 09:43:40 +0000

    Fixed additional cause of 10 in category label. Fixes #7

--- addons/apps/jw_locator/admin.php
+++ addons/apps/jw_locator/admin.php
@@ -6,10 +6,10 @@ if ($CurrentUser->logged_in() && $CurrentUser->has_priv('jw_locator')) {
     $this->add_create_page('jw_locator', 'edit');
 
     $this->add_setting('jw_locator_batch_size', 'Batch Size', 'select', 25, array(
-        array('label' => '10', 'value' => 10),
-        array('label' => '25', 'value' => 25),
-        array('label' => '50', 'value' => 50),
-        array('label' => '100', 'value' => 100)
+        array('label' => '10 Locations', 'value' => 10),
+        array('label' => '25 Locations', 'value' => 25),
+        array('label' => '50 Locations', 'value' => 50),
+        array('label' => '100 Locations', 'value' => 100)
     ));
 
     $this->add_setting('jw_locator_google_api_key', 'Google API Key', 'text');
