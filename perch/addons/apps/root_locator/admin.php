4aff69d77eeb8f70294f0cdb5d9867c6839aa2a9
James Wigger <james@rootstudio.co.uk>
2017-07-26 16:54:40 +0100

    Added initial support for Perch Content Importer API

--- perch/addons/apps/root_locator/admin.php
+++ perch/addons/apps/root_locator/admin.php
@@ -2,7 +2,7 @@
 
 if ($CurrentUser->logged_in() && $CurrentUser->has_priv('root_locator')) {
     $this->register_app('root_locator', 'Locator', 1, 'Provide location based listings for your site', '3.0.0');
-    $this->require_version('root_locator', '3.0');
+    $this->require_version('root_locator', '3.0.3');
     $this->add_create_page('root_locator', 'edit');
 
     $this->add_setting('root_locator_batch_size', 'Batch Size', 'select', 25, array(
@@ -14,17 +14,7 @@ if ($CurrentUser->logged_in() && $CurrentUser->has_priv('root_locator')) {
 
     $this->add_setting('root_locator_google_api_key', 'API key', 'text');
 
-    include(__DIR__ . '/lib/vendor/autoload.php');
-
-    spl_autoload_register(function ($class_name) {
-        if (strpos($class_name, 'RootLocator') === 0) {
-            include(PERCH_PATH . '/addons/apps/root_locator/lib/' . $class_name . '.class.php');
-
-            return true;
-        }
-
-        return false;
-    });
+    require __DIR__ . '/autoloader.php';
 
     // Search handler
     PerchSystem::register_admin_search_handler('RootLocator_SearchHandler');
--- perch/addons/apps/root_locator/scheduled_tasks.php
+++ perch/addons/apps/root_locator/scheduled_tasks.php
@@ -4,17 +4,7 @@ PerchScheduledTasks::register_task('root_locator', 'geocode_location_batch', 10,
 
 function root_locator_process_task_queue($last_run_date)
 {
-    include(__DIR__ . '/lib/vendor/autoload.php');
-
-    spl_autoload_register(function ($class_name) {
-        if (strpos($class_name, 'RootLocator') === 0) {
-            include(PERCH_PATH . '/addons/apps/root_locator/lib/' . $class_name . '.class.php');
-
-            return true;
-        }
-
-        return false;
-    });
+    require __DIR__ . '/autoloader.php';
 
     $API = new PerchAPI(1.0, 'root_locator');
     $Tasks = new RootLocator_Tasks($API);
